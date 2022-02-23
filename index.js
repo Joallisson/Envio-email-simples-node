1c3dda56b9393323996659a8d9a54da703619971
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-02-23 20:55:06 -0300

    Finalizado
    versão 1

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const nodemailer = require("nodemailer")
 
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com", //Servidor de email
-    port: "", //A porta do servidor
-    secure: "", //Se tem ou não criptografia SSL ou TLS
+    port: n465, //A porta do servidor
+    secure: true, //Se tem ou não criptografia SSL ou TLS
     auth: { //Colocar uma conta de email com a senha
-        user: "", //Email
-        pass: "" //Senha
+        user: "emaildetestedojnc@gmail.com", //Email
+        pass: "Servepratudo1324'@" //Senha
     }
 })
 
 transporter.sendMail({
-    from: "", //Conata de email que está enviando a mensagem
-    to: "", //Conta de email que vai receber a mensagem
-    subject: "", //Título da mensagem
-    text: "", //Descrição da mensagem
-    html: "" //Código html opcional
+    from: "Joallisson Nascimento Castro <emaildetestedojnc@gmail.com>", //Conata de email que está enviando a mensagem
+    to: "joallissons@gmail.com", //Conta de email que vai receber a mensagem
+    subject: "Estou enviando esse email pelo backend", //Título da mensagem
+    //text: "Olhaí bixo, deu certo pra enviar email com o node, vou postar esse <a href='https://github.com/Joallisson/Envio-email-simples-node'>projeto</a> lá no github", //Descrição da mensagem
+    html: "Olhaí bixo, deu certo pra enviar email com o node, vou postar esse <a href='https://github.com/Joallisson/Envio-email-simples-node'>projeto</a> lá no github" //Código html opcional
 })
